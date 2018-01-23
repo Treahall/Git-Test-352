97055ad7ec721eb1c15eab173f3ee264e2126567
Treahall <treahall@ut.utm.edu>
2018-01-23 15:46:18 -0600

    Initial implementation as a Hello World program

--- ConsoleAppForGit/Program.cs
+++ ConsoleAppForGit/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleAppForGit
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
